9d883fb4a204817d8bbd96c0aea46ea789cb3e97
James Perkins <jrperkinsjr@gmail.com>
2016-01-13 20:39:24 +0000

    Fix build failure with invalid Client test package.

--- weather/src/test/java/com/chowhouse/Client.java
+++ weather/src/test/java/com/chowhouse/Client.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-package com.chowhouse.test;
+package com.chowhouse;
 
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
